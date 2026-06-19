f201804f93ac6a166c2bfc28b529b35b05fef906
TomaSuzuki <toma-suzuki@ma-vericks.com>
2026-06-19 15:51:59 +0900

    Update media link card to yusukebe.com

--- examples/basic/src/generated/decks/media/slide-5.ts
+++ examples/basic/src/generated/decks/media/slide-5.ts
@@ -13,14 +13,14 @@ function _createMdxContent(props) {
       children: "Link card"
     }), "\n", _jsx(_components.p, {
       children: _jsx(_components.a, {
-        href: "https://zenn.dev/ashunar0/articles/1ba94a110d8622",
-        children: "https://zenn.dev/ashunar0/articles/1ba94a110d8622"
+        href: "https://yusukebe.com/",
+        children: "https://yusukebe.com/"
       })
     }), "\n", _jsx(LinkCard, {
-      href: "https://zenn.dev/ashunar0/articles/1ba94a110d8622",
-      title: "Hono でバックエンド API を作るときの個人的ベストプラクティス",
-      image: "https://res.cloudinary.com/zenn/image/upload/s--n9GBaqZ7--/c_fit%2Cg_north_west%2Cl_text:notosansjp-medium.otf_55:Hono%2520%25E3%2581%25A7%25E3%2583%2590%25E3%2583%2583%25E3%2582%25AF%25E3%2582%25A8%25E3%2583%25B3%25E3%2583%2589%2520API%2520%25E3%2582%2592%25E4%25BD%259C%25E3%2582%258B%25E3%2581%25A8%25E3%2581%258D%25E3%2581%25AE%25E5%2580%258B%25E4%25BA%25BA%25E7%259A%2584%25E3%2583%2599%25E3%2582%25B9%25E3%2583%2588%25E3%2583%2597%25E3%2583%25A9%25E3%2582%25AF%25E3%2583%2586%25E3%2582%25A3%25E3%2582%25B9%2Cw_1010%2Cx_90%2Cy_100/g_south_west%2Cl_text:notosansjp-medium.otf_37:%25E3%2581%2582%25E3%2581%2595%25E3%2581%25B2%2Cx_203%2Cy_121/g_south_west%2Ch_90%2Cl_fetch:aHR0cHM6Ly9zdGF0aWMuemVubi5zdHVkaW8vdXNlci11cGxvYWQvYXZhdGFyLzZhN2JkYzMxMjIuanBlZw==%2Cr_max%2Cw_90%2Cx_87%2Cy_95/v1627283836/default/og-base-w1200-v2.png?_a=BACMTiAE",
-      siteName: "Zenn"
+      href: "https://yusukebe.com/",
+      title: "ゆーすけべー日記",
+      description: "ゆーすけべーの名前で活動しています。天然パーマです。",
+      image: "https://yusukebe.com/icons/myicon.png"
     }), "\n", _jsx(_components.p, {
       children: "Link cards resolve OGP metadata at compile time when available and stay script-free at runtime."
     })]
